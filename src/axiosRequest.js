d15606b79b197ab55d50d05bb53b824f5ddbc6c9
JUSTIN ALEQUIN <justinalequin@JUSTINs-MacBook-Pro.local>
2022-05-05 14:53:07 -0400

    updated

--- src/axiosRequest.js
+++ src/axiosRequest.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const axiosRequest = axios.create({
-  baseURL: "http://localhost:5100/api",
+  baseURL:
+    process.env.REACT_APP_AXIOS === "development"
+      ? "http://localhost:5100/api"
+      : "/api",
   withCredentials: true,
 });
 
